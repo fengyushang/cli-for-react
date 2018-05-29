1cc9cb779049b17c8c3766835bcefff5ad440b75
zhangy <zhangy@htrader.cn>
2018-05-29 17:42:13 +0800

    bugfix

--- src/index.js
+++ src/index.js
@@ -4,47 +4,51 @@ const fs = require('fs');
 const path = require('path');
 
 const projectName = process.argv[2];
+console.log(projectName);
 const root = path.resolve(projectName);
 
-let stat=fs.stat;
+let stat = fs.stat;
 
-const copy=function(src,dst){
+const copy = function (src, dst) {
+    console.log(src,dst);
     //读取目录
-    fs.readdir(src,function(err,paths){
-        if(err){
+    fs.readdir(src, function (err, paths) {
+        if (err) {
             throw err;
         }
-        paths.forEach(function(path){
-            const _src=src+'/'+path;
-            const _dst=dst+'/'+path;
+        paths.forEach(function (path) {
+            const _src = src + '/' + path;
+            const _dst = dst + '/' + path;
             let readable;
             let writable;
-            stat(_src,function(err,st){
-                if(err){
+            stat(_src, function (err, st) {
+                if (err) {
                     throw err;
                 }
 
-                if(st.isFile()){
-                    readable=fs.createReadStream(_src);//创建读取流
-                    writable=fs.createWriteStream(_dst);//创建写入流
+                if (st.isFile()) {
+                    readable = fs.createReadStream(_src);//创建读取流
+                    writable = fs.createWriteStream(_dst);//创建写入流
                     readable.pipe(writable);
-                }else if(st.isDirectory()){
-                    exists(_src,_dst,copy);
+                } else if (st.isDirectory()) {
+                    exists(_src, _dst, copy);
                 }
             });
         });
     });
 };
-const exists=function(src,dst,callback){
+const exists = function (src, dst, callback) {
     //测试某个路径下文件是否存在
-    fs.exists(dst,function(exists){
-        if(exists){//不存在
-            callback(src,dst);
-        }else{//存在
-            fs.mkdir(dst,function(){//创建目录
-                callback(src,dst)
+    fs.exists(dst, function (exists) {
+        if (exists) {//存在
+            callback(src, dst);
+        } else {//不存在
+            fs.mkdir(dst, function () {//创建目录
+                callback(src, dst)
             })
         }
     })
 };
-exists(path.join(__dirname, './lib'),root,copy);
+if(projectName){
+    exists(path.join(__dirname, './lib'), root, copy);
+}
